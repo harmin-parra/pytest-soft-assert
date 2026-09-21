a43d525b54be928e166d7b2a5d64041e9dab7c8a
harmin-parra <harmin.parra@gmail.com>
2026-09-21 23:21:42 +0200

    update

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 # html_theme = 'press'
 # html_theme = 'alabaster'
 
